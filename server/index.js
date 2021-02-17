5c3cdd00e724ffe189010bbd3e6a4611ddc79f70
jimbowler82 <j.bowler2017@gmail.com>
2021-02-17 17:09:21 +0000

    server sockets added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+const httpServer = require("http").createServer(app);
+const options = {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+};
+const io = require("socket.io")(httpServer, options);
+const cors = require("cors");
+
+app.use(cors());
+
+let drawingData = [];
 
 io.on("connection", (socket) => {
   console.log(`New connection: ${socket.id}`);
 
+  const data = drawingData;
+
+  // Send initial data to new connections
+  io.to(socket.id).emit("initialData", data);
+
+  // Receive new drawing input, save to drawingData, send out to all connections.
   socket.on("mouse", (data) => {
-    console.log("Mouse data received", data);
+    console.log("Mouse data received");
+    drawingData.push(data);
+
+    socket.broadcast.emit("mouse", data);
+  });
+
+  // Clear out drawingData on clearCanvas message
+  socket.on("clearCanvas", () => {
+    console.log("Clearing canvas");
+    database = [];
+    socket.broadcast.emit("clearCanvas");
   });
 });
 
-server.listen("5000", () => console.log("Server running on 5000"));
+httpServer.listen("5000", () => console.log("Server running on 5000"));
