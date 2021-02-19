5dad90fbe4572fe9bc213cd93002557ff166d8bb
jimbowler82 <j.bowler2017@gmail.com>
2021-02-19 15:36:01 +0000

    added auto room join/create & url

--- client/src/components/invite/index.js
+++ client/src/components/invite/index.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./invite.module.css";
 import { MdContentCopy } from "react-icons/md";
 import { Button, InputGroup, FormControl } from "react-bootstrap";
 
-export default function Invite({ url }) {
+export default function Invite({ room }) {
+  const base_url = `http://localhost:3000/`;
+  const url = base_url + room;
+
+  const inputRef = useRef(null);
+
+  function copyLink() {
+    inputRef.current.disabled = false;
+    inputRef.current.select();
+    document.execCommand("copy");
+    inputRef.current.disabled = true;
+    console.log("url copied");
+  }
+
   return (
     <div className={styles.inviteDiv}>
       <span>Invite Link:</span>
       <InputGroup className="mb-3" style={{ marginTop: "1rem" }}>
         <FormControl
-          value="http://yoursharablelink/random"
+          ref={inputRef}
+          value={url}
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
           disabled
           style={{ textAlign: "center" }}
         />
         <InputGroup.Append>
-          <Button variant="secondary">
+          <Button variant="secondary" onClick={copyLink}>
             <MdContentCopy />
           </Button>
         </InputGroup.Append>
--- client/src/components/mainPage/index.js
+++ client/src/components/mainPage/index.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../header";
 import Invite from "../invite";
 import Canvas from "../canvas";
 import ChatBox from "../chatBox";
 import styles from "./mainPage.module.css";
+import { useParams } from "react-router-dom";
+import useSocketContext from "../../context/socketContext";
 
 export default function MainPage() {
+  const [roomId, setRoomId] = useState("");
+  const [socket] = useSocketContext();
+  let { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+    if (id) {
+      socket.emit("joinExisitingRoom", { roomId: id });
+    } else {
+      socket.emit("createNewRoom");
+    }
+
+    socket.on("roomData", ({ roomId }) => {
+      // Add room name to user context.
+      console.log({ roomId });
+      setRoomId(roomId);
+    });
+
+    return () => {
+      socket.emit("leaveRoom"); // Need to add room name.
+      // Set listeners to off.
+      socket.off("roomData");
+    };
+  }, []);
+
   return (
     <main className={styles.main}>
       <Header />
-      <Invite />
+      <Invite room={roomId} />
       <Canvas />
       <ChatBox />
     </main>
